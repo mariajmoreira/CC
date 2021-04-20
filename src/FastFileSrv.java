66e91e35dbb2a1672dc71def9e7e2371e948e968
mjloirinha <61200595+mjloirinha@users.noreply.github.com>
2021-04-20 12:30:52 +0100

    Delete src directory

--- src/FastFileSrv.java
+++
@@ -1,8 +0,0 @@
-public class FastFileSrv {
-
-     //Contém as portas para o envio de PDU's.
-    private int protected_port, control_port;
-
-    //Endereço IP.
-    private String srv_address;
-}
--- src/HttpGw.java
+++
@@ -1,6 +0,0 @@
-public class HttpGw {
-
-    public static void main(String[] args) {
-
-    }
-}
